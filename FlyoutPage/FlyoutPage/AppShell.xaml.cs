18ac7a931e166e3fc9c39916722bbc5a7c19c33a
OMKAR9395 <omkarmohite85@gmail.com>
2025-06-10 13:48:02 +0530

    Project

---
+++ FlyoutPage/FlyoutPage/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlyoutPage
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FlyoutPage/FlyoutPage/FlyoutPageDemo.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace FlyoutPageApp
+{
+    public partial class FlyoutPageDemo : FlyoutPage
+    {
+        public FlyoutPageDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
